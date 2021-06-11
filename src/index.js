333c34f733b55d520ecbf7a60eb59a52ada7f34d
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-06-11 10:30:00 +0200

    bred

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import debounce from 'lodash.debounce';
 import { notice  } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import listPictures from './partials/list_pictures.hbs';
+
+
+const options = {
+    headers: {
+        Authorization: '',
+    },
+}
+const url =
+    'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ'
+
+fetch(url, options)
+    .then(res => res.json())
+    .then(console.log(res))
